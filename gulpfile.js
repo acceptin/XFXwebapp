1020337da4ce1af941211fc751412683cde666e7
Liang <451856399@qq.com>
2015-09-14 09:36:19 +0800

    修改工作流

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,59 @@
  */
 
 // 引入 gulp及组件
-var gulp    = require('gulp'),                 //基础库
-    htmlmin = require('gulp-htmlmin'),         //html压缩
-    imagemin = require('gulp-imagemin'),       //图片压缩
-    less = require('gulp-less'),          	   //less
-    minifycss = require('gulp-minify-css'),    //css压缩
-    jshint = require('gulp-jshint'),           //js检查
-    uglify  = require('gulp-uglify'),          //js压缩
-    rename = require('gulp-rename'),           //重命名
-    concat  = require('gulp-concat'),          //合并文件
-    clean = require('gulp-clean'),             //清空文件夹
-    tinylr = require('tiny-lr'),               //livereload
+var gulp    = require('gulp'),                          //基础库
+    htmlmin = require('gulp-htmlmin'),                  //html压缩
+    imagemin = require('gulp-imagemin'),                //图片压缩
+    less = require('gulp-less'),          	            //less
+    minifycss = require('gulp-minify-css'),             //css压缩
+    jshint = require('gulp-jshint'),                    //js检查
+    uglify  = require('gulp-uglify'),                   //js压缩
+    rename = require('gulp-rename'),                    //重命名
+    concat  = require('gulp-concat'),                   //合并文件
+    clean = require('gulp-clean'),                      //清空文件夹
+    tinylr = require('tiny-lr'),                        //livereload
     server = tinylr(),
     port = 35729,
-    livereload = require('gulp-livereload');   //livereload
+    livereload = require('gulp-livereload');            //livereload
+
+var imgSrc = './src/img/**/*',                          //图片路径
+    imgDst = './dist/img',
+    fontSrc = './bower_components/font-awesome/fonts/*',//字体路径
+    fontDst = './dist/fonts/',
+    jsSrc = './src/js/**/*.js',                         //js路径
+    jsDst = './dist/js',
+    htmlSrc = './src/*.html',                           //html路径
+    htmlDst = './dist/',
+    cssSrc = './src/less/bootstrap.less',               //样式路径
+    cssDst = './dist/css',
+    tplSrc = './src/templets/*.htm',
+    tplDst = './dist/templets';
+//bower
+var libSrc = [
+    './bower_components/bootstrap/js/transition.js',
+    './bower_components/bootstrap/js/dropdown.js',
+    './bower_components/bootstrap/js/collapse.js',
+    './bower_components/bootstrap/js/modal.js',
+    './bower_components/bootstrap/js/tab.js',
+    './bower_components/bootstrap/js/alert.js',
+    './bower_components/jasny-bootstrap/js/offcanvas.js'
+]
+var libDst = './src/js/vendor';
 
 // 字体处理
-gulp.task('font', function() {
-    var fontSrc = './bower_components/font-awesome/fonts/*',
-        fontDst = './dist/fonts/';
+gulp.task('font',['clean-font'], function() {
 
     gulp.src(fontSrc)
       .pipe(gulp.dest(fontDst));
 });
+//清理字体
+gulp.task('clean-font', function () {
+  return gulp.src('./dist/fonts', {read: false})
+    .pipe(clean());
+});
 
 // HTML处理
 gulp.task('html', function() {
-    var htmlSrc = './src/*.html',
-        htmlDst = './dist/';
-
     gulp.src(htmlSrc)
         .pipe(livereload(server))
         .pipe(gulp.dest(htmlDst));
@@ -40,13 +64,10 @@ gulp.task('html', function() {
 
 // 样式处理
 gulp.task('css', function () {
-    var cssSrc = './src/less/bootstrap.less',
-        cssDst = './dist/css';
-
     gulp.src(cssSrc)
-        .pipe(less({ style: 'expanded'}))
+        .pipe(less())
         .pipe(gulp.dest(cssDst))
-        .pipe(rename({ suffix: '.min' }))
+        .pipe(rename({basename:'xfx',suffix: '.min' }))
         .pipe(minifycss())
         .pipe(livereload(server))
         .pipe(gulp.dest(cssDst));
@@ -54,8 +75,6 @@ gulp.task('css', function () {
 
 // 图片处理
 gulp.task('images', function(){
-    var imgSrc = './src/img/**/*',
-        imgDst = './dist/img';
     gulp.src(imgSrc)
         .pipe(imagemin())
         .pipe(livereload(server))
@@ -64,29 +83,9 @@ gulp.task('images', function(){
 
 // js处理
 gulp.task('js', function () {
-    var jsSrc = './src/js/**/*.js',
-        jsDst = './dist/js';
     //将bower的库文件对应到指定位置
-    gulp.src('./bower_components/bootstrap/js/transition.js')
-      .pipe(gulp.dest('./src/js/vendor'));
-
-	gulp.src('./bower_components/bootstrap/js/dropdown.js')
-      .pipe(gulp.dest('./src/js/vendor'));
-
-	gulp.src('./bower_components/bootstrap/js/collapse.js')
-      .pipe(gulp.dest('./src/js/vendor'));
-
-	gulp.src('./bower_components/bootstrap/js/modal.js')
-      .pipe(gulp.dest('./src/js/vendor'));
-
-	gulp.src('./bower_components/bootstrap/js/tab.js')
-      .pipe(gulp.dest('./src/js/vendor'));
-
-	gulp.src('./bower_components/bootstrap/js/alert.js')
-      .pipe(gulp.dest('./src/js/vendor'));
-
-	gulp.src('./bower_components/jasny-bootstrap/js/offcanvas.js')
-      .pipe(gulp.dest('./src/js/vendor'));
+    gulp.src(libSrc)
+      .pipe(gulp.dest(libDst));
 
     gulp.src(jsSrc)
         //.pipe(jshint('.jshintrc'))
@@ -100,19 +99,22 @@ gulp.task('js', function () {
 });
 
 // 模版文件HTML处理
-gulp.task('tpl', function() {
-    var tplSrc = './src/templets/*.htm',
-        tplDst = './dist/templets';
+gulp.task('tpl', ['clean-tpl'] ,function() {
 
     gulp.src(tplSrc)
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest(tplDst));
 });
+//清理模版
+gulp.task('clean-tpl', function () {
+  return gulp.src('./dist/templets', {read: false})
+    .pipe(clean());
+});
 
 // 清空图片、样式、js
 gulp.task('clean', function() {
-    gulp.src(['./dist/css', './dist/js', './dist/images', './dist/templets'], {read: false})
-        .pipe(clean({focus:true}));
+    gulp.src(['./dist/images','./dist/css', './dist/js'])
+        .pipe(clean({force: true}));
 });
 
 
